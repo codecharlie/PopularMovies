5b2fb4a8d736321c13f31341a51a5574dff034ad
Charles Morgan <charles@polymorphic-solutions.com>
2015-08-22 20:44:26 -0500

    Initial Add
    Initial Add to the Project

---
+++ app/src/main/java/com/polymorphic_solutions/popularmovies/Movie.java
@@ -0,0 +1,10 @@
+package com.polymorphic_solutions.popularmovies;
+
+/**
+ * Created by cmorgan on 8/22/15.
+ *
+ * The Movie Class will hold all the on information about the individual movie
+ */
+public class Movie {
+    
+}
